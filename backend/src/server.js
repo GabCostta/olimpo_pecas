562fae3db45a970cfb88604f8bc1f4012c3d9042
Nicolas Firmino <nicolasneuma10@gmail.com>
2025-03-21 19:47:17 -0300

    Criação de Rotas + Estilização da logo do carrinho

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 
 // Rotas
 app.use("/users", userRoutes);
+app.use("/produtos", productRoutes);
+app.use("/carrinho", cartRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
