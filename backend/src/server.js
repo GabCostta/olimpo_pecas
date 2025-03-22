9eeb6e3348d1080d5ef36f820d41491bbf36ce74
Nicolas Firmino <nicolasneuma10@gmail.com>
2025-03-22 13:26:25 -0300

    atualizando server

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,7 @@ import { PrismaClient } from "@prisma/client";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
-<<<<<<< HEAD
-=======
 import orderRoutes from "./routes/orderRoutes.js";
->>>>>>> teste
 
 dotenv.config();
 
@@ -21,15 +18,12 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/produtos", productRoutes);
 app.use("/carrinho", cartRoutes);
-<<<<<<< HEAD
-=======
 app.use("/orders", orderRoutes); // Rota de pedidos
 
 app.get("/", (req, res) => {
     res.send("Servidor está rodando corretamente!");
 });
 
->>>>>>> teste
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
