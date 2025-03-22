1914f466d46e6b10929bc137db29f83881503b0b
sallys03 <sallysferreira03@gmail.com>
2025-03-21 23:30:13 -0300

    reparos adicionais

--- frontend/src/ui/components/Header/Header.jsx
+++ frontend/src/ui/components/Header/Header.jsx
@@ -17,14 +17,14 @@ function Header() {
               <button type="button"><Link to="/Login">Entrar</Link></button>
               <Link className="link-carrinho-nav" to="/Cart"><img src={carrinho} alt="carrinho" className="icon-carrinho" /></Link>
             </div>
-            <div className="nav-footer">
+          </div>
+        </nav>
+        <div className="nav-footer">
               <li><Link to="/" className="nav-footer-home">Home</Link></li>
               <li><Link to="/ProductList" className="nav-footer-produtos">Produtos</Link></li>
               <li><Link to="/ProductView" className="nav-footer-categorias">Categorias</Link></li>
               <li><Link to="/Cart" className="nav-footer-pedidos">Carrinho</Link></li>
             </div>
-          </div>
-        </nav>
       </header>
     </>
   );
